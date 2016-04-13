012d19ce1a3ae098e21b4e42cbf765b683ff763a
devweb.eu <vhost45490ssh@regulus.elkdata.ee>
2016-04-13 07:48:43 +0000

    jquery and html

---
+++ feed.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  getTweets();
+
+});
+
+function getTweets() {
+  $.ajax({
+    url: "getfeed.php",
+    success: function(data) {
+      var array = JSON.parse(data).statuses;
+      console.log(array);
+      printTweets(array);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+function printTweets(tweets) {
+
+  var html = '';
+
+  $(tweets).each(function(i, tweet) {
+
+
+    //html += '<div>' + i + '</div>';
+
+    html += '<div class="item">'+
+
+              '<div class="profile-image" style="background-image: url(' + tweet.user.profile_image_url + ');"></div>'+
+              '<p>' + tweet.user.name + '</p>'+
+              '<p>' + tweet.text + '</p>'+
+
+            '</div>';
+  });
+
+  $("#content").append( $(html) );
+}
